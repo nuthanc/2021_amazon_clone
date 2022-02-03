58cb148212f4406d6b3866bde09d86a58af6afb0
Nuthan Chandra <nuthanc@juniper.net>
2022-02-03 16:12:40 +0530

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      Hello, I'm header
-    </div>
-  )
-}
+    <nav className="Header">
+      <img
+        className="Header-logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+      />
+      <input className="Header-search" />
+      <div>
+        <span>Hello, Sign in</span>
+        <span>Accounts & Lists</span>
+      </div>
+      <div>
+        <span>Returns</span>
+        <span>& Orders</span>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
